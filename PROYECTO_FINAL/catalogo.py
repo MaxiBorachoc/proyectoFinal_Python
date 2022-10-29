1703aacd925dc336bb8e38cea6ed85a179c41958
Maxi <maxi.borachoc@gmail.com>
2022-10-29 03:08:42 -0300

    func volver solucionado

--- PROYECTO_FINAL/catalogo.py
+++ PROYECTO_FINAL/catalogo.py
@@ -1,152 +1,171 @@
 from tkinter import *
 from tkinter import ttk
-from PIL import Image
+import functools
+from PIL import Image, ImageTk
 
-raiz = Tk()
-raiz.geometry('1400x800')
-raiz.title("CATALOGO DE PRODUCTOS")
-raiz.resizable(False, False)
-raiz.config(bg="RoyalBlue3")
-raiz.iconbitmap('PROYECTO_FINAL/assets/carrito.ico')
+def initCat(parent):
+    parent.withdraw()
+    root = Toplevel(parent)
+    root.geometry('1000x700')
+    root.resizable(False, False)
+    root.title("CATALOGO DE PRODUCTOS")
+    root.config(bg="RoyalBlue3")
+    root.iconbitmap('PROYECTO_FINAL/assets/carrito.ico')
+    root.protocol("WM_DELETE_WINDOW", functools.partial(volver, parent, root))
 
-def combo():
-    lista2=Label(raiz, text="MODELO", font=("Bebas Neue", 22), bg="RoyalBlue3")
-    lista2.grid(row=1, column=1,padx=80, pady=10)
+    img1 = Image.open("PROYECTO_FINAL/assets/alternador.png")
+    img2 = Image.open("PROYECTO_FINAL/assets/arranque.png")
+    img3 = Image.open("PROYECTO_FINAL/assets/faro.png")
 
-    labelTitle.place_forget()
-    
-    opcion_model=StringVar()
-    model=("mod1", "mod2", "mod3")
+    img_alter= ImageTk.PhotoImage(img1)
+    img_arr= ImageTk.PhotoImage(img2)
+    img_faro= ImageTk.PhotoImage(img3)
 
-    opcion_model_tipo=StringVar()
-    model_tipo=("modelo1", "modelo2", "modelo4", "modelo5")
+    labelTitle = Label(root, text="SELECCIONE EL TIPO DE PRODUCTO", font=("Bebas Neue", 44), bg="RoyalBlue3", fg="IndianRed3")
+    labelTitle.place(x=175, y=30)
 
-    combobox3=ttk.Combobox(raiz, width=30, textvariable=opcion_model, values=model)
-    combobox3.current(0)
-    combobox3.grid(row=1, column=2)
-    
-    combobox3_1=ttk.Combobox(raiz, width=30, textvariable=opcion_model_tipo, values=model_tipo)
-    combobox3_1.current(0)
-    combobox3_1.grid(row=1, column=3)
+    imgArr=Label(root, image=img_arr, bg="RoyalBlue3")
+    imgArr.place(x=640, y=130)
 
-    lista1=Label(raiz, text="TIPO", font=("Bebas Neue", 22), bg="RoyalBlue3")
-    lista1.grid(row=3, column=1,padx=80, pady=10)
+    imgAlter=Label(root, image=img_alter, bg="RoyalBlue3")
+    imgAlter.place(x=640, y=340)
 
-    opcion_tipo=StringVar()
-    alterTipo=("Indiel", "Valeo", "Audi")
+    imgLuc=Label(root, image=img_faro, bg="RoyalBlue3", width=300, height=150)
+    imgLuc.place(x=620, y=550)
 
-    combobox=ttk.Combobox(raiz, width=30, textvariable=opcion_tipo, values=alterTipo)
-    combobox.current(0)
-    combobox.grid(row=3, column=2)
+    def combo():
+        lista2=Label(root, text="MODELO", font=("Bebas Neue", 22), bg="RoyalBlue3")
+        lista2.grid(row=1, column=1)
 
-    lista2=Label(raiz, text="MARCA", font=("Bebas Neue", 22), bg="RoyalBlue3")
-    lista2.grid(row=4, column=1,padx=80, pady=10)
+        labelTitle.place_forget()
+        
+        opcion_model=StringVar()
+        model=("mod1", "mod2", "mod3")
 
-    opcion_marca=StringVar()
-    marca=("Luces bajas", "Luces altas", "Guiños", "Auxiliares", "Stop", "Marcha Atrás")
+        opcion_model_tipo=StringVar()
+        model_tipo=("modelo1", "modelo2", "modelo4", "modelo5")
 
-    combobox2=ttk.Combobox(raiz, width=30, textvariable=opcion_marca, values=marca)
-    combobox2.current(0)
-    combobox2.grid(row=2, column=2)
+        combobox3=ttk.Combobox(root, width=30, textvariable=opcion_model, values=model)
+        combobox3.current(0)
+        combobox3.grid(row=1, column=2)
+        
+        combobox3_1=ttk.Combobox(root, width=30, textvariable=opcion_model_tipo, values=model_tipo)
+        combobox3_1.current(0)
+        combobox3_1.grid(row=1, column=3)
 
-    lista2=Label(raiz, text="AÑO", font=("Bebas Neue", 22), bg="RoyalBlue3")
-    lista2.grid(row=2, column=1,padx=80, pady=10)
+        lista1=Label(root, text="TIPO", font=("Bebas Neue", 22), bg="RoyalBlue3")
+        lista1.grid(row=3, column=1,padx=80, pady=10)
 
-    opcion=StringVar()
-    anio=("1990-95", "1996-00", "2001-05", "2006-2010", "2011-15", "2016-2022")
+        opcion_tipo=StringVar()
+        alterTipo=("Indiel", "Valeo", "Audi")
 
-    combobox4=ttk.Combobox(raiz, width=30, textvariable=opcion, values=anio)
-    combobox4.current(0)
-    combobox4.grid(row=4, column=2)
+        combobox=ttk.Combobox(root, width=30, textvariable=opcion_tipo, values=alterTipo)
+        combobox.current(0)
+        combobox.grid(row=3, column=2)
 
-    cuadro_añadir = LabelFrame(raiz, width=7, height=4, bg="snow", bd=".5px")
-    cuadro_añadir.place(x=650, y=450)
+        lista2=Label(root, text="MARCA", font=("Bebas Neue", 22), bg="RoyalBlue3")
+        lista2.grid(row=4, column=1,padx=80, pady=10)
 
-    ##labelPedir = Label(cuadro_añadir, text="Añadir al pedido", width=15, height=4, bg="gray75", font=("", 9))
-    ##labelPedir.pack()
+        opcion_marca=StringVar()
+        marca=("Luces bajas", "Luces altas", "Guiños", "Auxiliares", "Stop", "Marcha Atrás")
 
-    botonPedir = Button(cuadro_añadir, text="Añadir al pedido", width=15, height=4, bg="medium sea green")
-    botonPedir.pack()
-    contador = IntVar()
-    contador= 0
-    botonCarrito = Button(raiz, text=contador, width=6, height=2, bg="snow", font=("", 14))
-    botonCarrito.place(x=24, y=700)
+        combobox2=ttk.Combobox(root, width=30, textvariable=opcion_marca, values=marca)
+        combobox2.current(0)
+        combobox2.grid(row=2, column=2)
 
-def arranque():
-    botonAlter.grid_forget()
-    imgAlter.place_forget()
-    imgArr.place_forget()
-    imgLuc.place_forget()
-    botonLuces.grid_forget()
+        lista2=Label(root, text="AÑO", font=("Bebas Neue", 22), bg="RoyalBlue3")
+        lista2.grid(row=2, column=1,padx=80, pady=10)
 
-    botonArranque.grid_forget()
+        opcion=StringVar()
+        anio=("1990-95", "1996-00", "2001-05", "2006-2010", "2011-15", "2016-2022")
 
-    botonAtras = Button(raiz, text="<-", width=10, height=2, bg="snow")
-    botonAtras.place(x=1350, y=700)
+        combobox4=ttk.Combobox(root, width=30, textvariable=opcion, values=anio)
+        combobox4.current(0)
+        combobox4.grid(row=4, column=2)
 
-    labelPedidoActual = Label(raiz, text="CATALOGO PARA MOTOR DE ARRANQUE", width=30, height=1, bg="RoyalBlue3", font=("Bebas Neue", 28), fg="IndianRed3")
-    labelPedidoActual.grid(row=0, column=2, pady=50, padx=180)
+        cuadro_añadir = LabelFrame(root, width=7, height=4, bg="snow", bd=".5px")
+        cuadro_añadir.place(x=425, y=590)
 
-    combo()
+        labelPedir = Label(cuadro_añadir, text="Añadir al pedido", width=15, height=4, bg="gray75", font=("", 9))
+        labelPedir.pack()
 
-def alter():
-    botonAlter.grid_forget()
-    botonLuces.grid_forget()
-    botonArranque.grid_forget()
-    imgAlter.place_forget()
-    imgArr.place_forget()
-    imgLuc.place_forget()
-    botonAtras = Button(raiz, text="<-", width=10, height=2, bg="snow")
-    botonAtras.place(x=1350, y=700)
-   
-    labelPedidoActual = Label(raiz, text="CATALOGO PARA ALTERNADOR", width=30, height=1, bg="RoyalBlue3", font=("Bebas Neue", 28), fg="IndianRed3")
-    labelPedidoActual.grid(row=0, column=2, pady=50, padx=180)
-    combo()
-   
+        ##botonPedir = Button(raiz, text="Añadir al pedido", width=15, height=4, bg="medium sea green")
+        ##botonPedir.place(x=425, y=590)
+        contador = IntVar()
+        contador= 0
+        botonCarrito = Button(root, text=contador, width=5, height=2, bg="snow", font=("", 14))
+        botonCarrito.place(x=6, y=600)
 
-def luces():
-    botonAlter.grid_forget()
-    botonLuces.grid_forget()
-    botonArranque.grid_forget()
-    imgAlter.place_forget()
-    imgArr.place_forget()
-    imgLuc.place_forget()
-    botonAtras = Button(raiz, text="<-", width=10, height=2, bg="snow")
-    botonAtras.place(x=1350, y=700)
+    def arranque():
+        botonAlter.grid_forget()
+        imgAlter.place_forget()
+        imgArr.place_forget()
+        imgLuc.place_forget()
+        botonLuces.grid_forget()
+
+        botonArranque.grid_forget()
+
+        botonAtras = Button(root, text="<-", width=12, height=3, bg="snow")
+        botonAtras.place(x=900, y=600)
+
+        labelPedidoActual = Label(root, text="CATALOGO PARA MOTOR DE ARRANQUE", width=30, height=1, bg="RoyalBlue3", font=("Bebas Neue", 28), fg="IndianRed3")
+        labelPedidoActual.grid(row=0, column=2, pady=50, padx=50)
+
+        combo()
+
+    def alter():
+        botonAlter.grid_forget()
+        botonLuces.grid_forget()
+        botonArranque.grid_forget()
+        imgAlter.place_forget()
+        imgArr.place_forget()
+        imgLuc.place_forget()
+        botonAtras = Button(root, text="<-", width=12, height=3, bg="snow")
+        botonAtras.place(x=900, y=600)
+
+        labelPedidoActual = Label(root, text="CATALOGO PARA ALTERNADOR", width=30, height=1, bg="RoyalBlue3", font=("Bebas Neue", 28), fg="IndianRed3")
+        labelPedidoActual.grid(row=0, column=2, pady=50, padx=50)
+        combo()
+
+
+    def luces():
+        botonAlter.grid_forget()
+        botonLuces.grid_forget()
+        botonArranque.grid_forget()
+        imgAlter.place_forget()
+        imgArr.place_forget()
+        imgLuc.place_forget()
+        botonAtras = Button(root, text="<-", width=12, height=3, bg="snow")
+        botonAtras.place(x=900, y=600)
+
+        
+        labelPedidoActual = Label(root, text="CATALOGO PARA LUCES", width=30, height=1, bg="RoyalBlue3", font=("Bebas Neue", 28), fg="IndianRed3")
+        labelPedidoActual.grid(row=0, column=2, pady=50, padx=50)
+
+        combo()
+
+            
   
-    
-    labelPedidoActual = Label(raiz, text="CATALOGO PARA LUCES", width=30, height=1, bg="RoyalBlue3", font=("Bebas Neue", 28), fg="IndianRed3")
-    labelPedidoActual.grid(row=0, column=2, pady=50, padx=180)
 
-    combo()
+    botonArranque = Button(root,text="MOTOR DE ARRANQUE", bg="IndianRed3", command=arranque, font=("Bebas Neue", 18), width=30)
+    botonArranque.grid(row=1, column=0, pady=160, padx=140)
+
+    botonAlter = Button(root,text="ALTERNADOR",  bg="IndianRed3", command=alter, font=("Bebas Neue", 18), width=30)
+    botonAlter.grid(row=2, column=0, padx=140)
+
+
+    botonLuces = Button(root, text="LUCES", bg="IndianRed3", command=luces, font=("Bebas Neue", 18), width=30)
+    botonLuces.grid(row=3, column=0, pady=150, padx=140)
+
+
+    botonAtras = Button(root, text="<-", width=12, height=3, bg="snow", command=functools.partial(volver, parent, root))
+    botonAtras.place(x=900, y=600)
    
-img_alter= PhotoImage(file="PROYECTO_FINAL/assets/alternador.png")
-img_arr= PhotoImage(file="PROYECTO_FINAL/assets/arranque.png")
-img_faro= PhotoImage(file="PROYECTO_FINAL/assets/faro.png")
-
-labelTitle = Label(raiz, text="SELECCIONE EL TIPO DE PRODUCTO", font=("Bebas Neue", 44), bg="RoyalBlue3", fg="IndianRed3")
-labelTitle.place(x=390, y=30)
-
-imgArr=Label(raiz, image=img_arr, bg="RoyalBlue3")
-imgArr.place(x=840, y=140)
-
-imgAlter=Label(raiz, image=img_alter, bg="RoyalBlue3")
-imgAlter.place(x=840, y=350)
-
-imgLuc=Label(raiz, image=img_faro, bg="RoyalBlue3", width=300, height=150)
-imgLuc.place(x=820, y=560)
-
-botonArranque = Button(raiz,text="MOTOR DE ARRANQUE", bg="IndianRed3", command=arranque, font=("Bebas Neue", 23), width=30)
-botonArranque.grid(row=1, column=0, pady=160, padx=140)
-
-botonAlter = Button(raiz,text="ALTERNADOR",  bg="IndianRed3", command=alter, font=("Bebas Neue", 23), width=30)
-botonAlter.grid(row=2, column=0, padx=140)
+    
+    root.mainloop()
 
 
-botonLuces = Button(raiz, text="LUCES", bg="IndianRed3", command=luces, font=("Bebas Neue", 23), width=30)
-botonLuces.grid(row=3, column=0, pady=150, padx=140)
-
-
-
-
-raiz.mainloop()
+def volver(parent, root):
+    parent.deiconify()
+    root.destroy()
+  
--- PROYECTO_FINAL/home.py
+++ PROYECTO_FINAL/home.py
@@ -1,38 +1,43 @@
 from tkinter import *
-
+import catalogo
+from PIL import Image, ImageTk
 
 raiz = Tk()
-raiz.geometry('1400x800')
+raiz.geometry('1000x700')
 raiz.title("CATALOGO DE PRODUCTOS")
 raiz.resizable(False, False)
 raiz.config(bg="snow")
 raiz.iconbitmap('PROYECTO_FINAL/assets/carrito.ico')
 
-img_fondo=PhotoImage(file="PROYECTO_FINAL/assets/fondo.png")
+
+imgFondo = Image.open("PROYECTO_FINAL/assets/fondo.png")
+img_fondo=ImageTk.PhotoImage(imgFondo)
 
 imgArr=Label(raiz, image=img_fondo)
 imgArr.place(x=0, y=0)
 
 labelTitle = Label(raiz, text="BIENVENIDO A NUESTRO PORTAL DE VENTA", font=("Bebas Neue", 44), bg="snow", fg="RoyalBlue3")
-labelTitle.place(x=370, y=100)
+labelTitle.place(x=145, y=100)
 labelSub = Label(raiz, text="INGRESE Y HAGA SU PEDIDO", font=("Bebas Neue", 44), bg="snow", fg="RoyalBlue3")
-labelSub.place(x=485, y=600)
+labelSub.place(x=260, y=540)
+
+def irCatalogo(*args):
+    catalogo.initCat(raiz)
 
 
-botonCatalogo = Button(raiz,text="VER CATÁLOGO", width=30, height=5, bg="RoyalBlue3", font=("Bebas Neue", 19))
-botonCatalogo.grid(row=0, column=0, pady=320, padx=140)
+botonCatalogo = Button(raiz,text="VER CATÁLOGO", width=30, height=5, bg="RoyalBlue3", font=("Bebas Neue", 12), command=irCatalogo)
+botonCatalogo.grid(row=0, column=0, pady=320, padx=110)
 
 
-botonUltPedido = Button(raiz,text="ÚLTIMO PEDIDO", width=30, height=5, bg="RoyalBlue3", font=("Bebas Neue", 19))
+botonUltPedido = Button(raiz,text="ÚLTIMO PEDIDO", width=30, height=5, bg="RoyalBlue3", font=("Bebas Neue", 12))
 botonUltPedido.grid(row=0, column=1, pady=300)
 
 cuadro = LabelFrame(raiz, width=30, height=5, bg="snow")
-cuadro.grid(row=0, column=2, pady=300, padx=160)
-labelPedidoActual = Label(cuadro, text="PEDIDO ACTUAL VACÍO", width=30, height=5, bd="1px", bg="RoyalBlue3", font=("Bebas Neue", 19))
+cuadro.grid(row=0, column=2, pady=300, padx=120)
+labelPedidoActual = Label(cuadro, text="PEDIDO ACTUAL VACÍO", width=30, height=5, bd="1px", bg="RoyalBlue3", font=("Bebas Neue", 12))
 labelPedidoActual.pack()
 
-botonPedidoActual = Button(raiz,text="PEDIDO ACTUAL", width=30, height=5, bg="RoyalBlue3", font=("Bebas Neue", 19))
-botonPedidoActual.grid(row=0, column=2, pady=300, padx=160)
-
+##botonPedidoActual = Button(raiz,text="PEDIDO ACTUAL", width=30, height=5, bg="RoyalBlue3", font=("Bebas Neue", 12))
+##botonPedidoActual.grid(row=0, column=2, pady=300, padx=120)
 
 raiz.mainloop()
